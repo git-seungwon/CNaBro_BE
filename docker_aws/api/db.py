2bbc679b8ab61e823fdb9028c71f7a4ddd5cc893
git-seungwon <tmddnjs3369@naver.com>
2024-09-23 14:30:32 +0900

    DB url 변경

--- docker_aws/api/db.py
+++ docker_aws/api/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
 # DB_URL = "mysql+pymysql://admin:cnabrobe!@database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com:3306/demo?charset=utf8"
-DB_URL = "database://admin:cnabrobe!@database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com:3306?charset=utf8"
+DB_URL = "mysql+pymysql://admin:cnabrobe!@database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com:3306?charset=utf8"
+
+#arn:aws:rds:ap-northeast-2:730335663316:db:database
+
 #mysql -u admin -p -h database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com
 #dialect+driver://username:password@host:port/database
 
