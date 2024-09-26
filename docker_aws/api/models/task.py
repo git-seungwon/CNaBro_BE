3aeeda3b5525281bc86c83f19d36907820bd2247
mina2o0 <rosena0226@naver.com>
2024-09-26 21:03:43 +0900

    데이터 입력

--- docker_aws/api/models/task.py
+++ docker_aws/api/models/task.py
@@ -25,4 +25,30 @@ class user(Base):
     user_id = Column(Integer, primary_key=True)
     user_nickname = Column(String(10))
     email = Column(String(30))
+    password = Column(String(20))
+    mobile_number = Column(String(11))
+    login_at = Column(datetime)
+    logout_at = Column(datetime)
+    create_at = Column(datetime)
+    update_at = Column(datetime)
+
+class note(Base):
+    __tablename__ = "note"
+
+    id = Column(int, primary_key=True)
+    user_id = Column(Integer)
+    content = Column(String)
+    start_time = Column(datetime)
+    end_time = Column(datetime)
+    creat_at = Column(datetime)
+    edit_time = Column(datetime)
+    score = Column(int(2))
+
+class tage(Base):
+    __tablename__ = "tag"
+
+    id = Column(int, primary_key=True)
+    note_id = Column(Integer)
+    tag_name = Column(String(8))
+
 
--- docker_aws/api/schemas/task.py
+++ docker_aws/api/schemas/task.py
@@ -24,7 +24,12 @@ from typing import List, Optional
 class user(BaseModel):
     user_nickname : Optional[str] 
     email : Optional[str]
-
+    password : Optional[str]
+    mobile_number : Optional[str]
+    login_at : datetime
+    logout_at : datetime
+    create_at : Optional[datetime]
+    updated_at : Optional[datetime]
 
 class userCreate(user):
     pass
@@ -34,3 +39,16 @@ class userCreateResponse(userCreate):
 
     class Config:
         orm_mode = True
+
+class note(BaseModel):
+    user_id : int
+    content : Optional[str]
+    start_time : Optional[datetime]
+    end_time : Optional[datetime]
+    create_at : Optional[datetime]
+    edit_at : datetime
+    score : Optional[str]
+
+class tag(BaseModel) :
+    note_id : int
+    tag_name : Optional[str]
