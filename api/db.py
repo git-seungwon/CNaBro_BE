6755419faea4a46e92c7f2aef264ffaba7a3746c
git-seungwon <tmddnjs3369@naver.com>
2024-10-02 21:15:01 +0900

    db url edit

--- api/db.py
+++ api/db.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from urllib.parse import quote
 
-user = "admin"
-pwd = "cnabrobe!"
-host = "database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com"
-port = 3306
-DB_URL = f'mysql+pymysql://{user}:{quote(pwd)}@{host}:{port}/demo?charset=utf8mb4'
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+user = os.environ["user"]
+pwd = os.environ["pwd"]
+host = os.environ["host"]
+port = os.environ["port"]
 
 db_engine = create_engine(DB_URL, echo=True)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
--- api/migrate_db.py
+++ api/migrate_db.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from api.models.task import Base
 from urllib.parse import quote
 
-user = "admin"
-pwd = "cnabrobe!"
-host = "database.cd8wseaayh6t.ap-northeast-2.rds.amazonaws.com"
-port = 3306
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+user = os.environ["user"]
+pwd = os.environ["pwd"]
+host = os.environ["host"]
+port = os.environ["port"]
+
 DB_URL = f'mysql+pymysql://{user}:{quote(pwd)}@{host}:{port}/demo?charset=utf8mb4'
 
 engine = create_engine(DB_URL, echo=True)
