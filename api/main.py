7a354278a38f539ce1584b60e9d15652d699c433
git-seungwon <tmddnjs3369@naver.com>
2024-10-11 12:41:55 +0900

    오타 일부 수정

--- api/main.py
+++ api/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
 import os
 
-from domain.user import user_routers
+from api.domain.user import user_routers
 
 app = FastAPI()
 
 @app.get("/version")
 def root():
     return {"version": os.getenv("version")}
-
-
 @app.get("/")
 def main():
     return {"message": "main_page!"}
 
-app.includ_router(user_routers.router)
+app.include_router(user_routers.router)
